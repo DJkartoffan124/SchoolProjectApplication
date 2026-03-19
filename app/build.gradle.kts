2e0689c4af4529720e063a2cd575c3895583199d
DJkartoffan124 <korolevarsenij2@gmail.com>
2026-03-19 23:33:32 +0300

    change app version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.arseniy.libraryapp"
         minSdk = 34
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.1.0 • Polished"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
