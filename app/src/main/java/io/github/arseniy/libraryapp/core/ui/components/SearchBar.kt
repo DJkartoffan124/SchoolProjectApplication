721b1c3dfcb35a9d50e6435631fad5de4826dc20
DJkartoffan124 <korolevarsenij2@gmail.com>
2026-03-19 22:06:58 +0300

    delete unnecessary coloring

--- app/src/main/java/io/github/arseniy/libraryapp/core/ui/components/SearchBar.kt
+++ app/src/main/java/io/github/arseniy/libraryapp/core/ui/components/SearchBar.kt
@@ -2,11 +2,9 @@ package io.github.arseniy.libraryapp.core.ui.components
 
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.OutlinedTextField
-import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 
 @Composable
 fun SearchBar(
@@ -18,14 +16,6 @@ fun SearchBar(
         value = query,
         onValueChange = onQueryChange,
         modifier = Modifier.fillMaxWidth(),
-        singleLine = true,
-        colors = OutlinedTextFieldDefaults.colors(
-            focusedBorderColor = Color.Red,
-            unfocusedBorderColor = Color.Gray,
-            focusedLabelColor = Color.Red,
-            cursorColor = Color.Red,
-            focusedTextColor = Color.Black,
-            unfocusedTextColor = Color.DarkGray
-        )
+        singleLine = true
     )
 }
